c510668dfb8ef5628fae07a1e17026397d4fdfb2
Soham Dasugpta <dasgupta8@wisc.edu>
2020-03-24 14:49:50 -0500

    added subscriptions

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -2,7 +2,55 @@ import React, { Component } from 'react'
 import Link from './Link'
 import { gql } from 'apollo-boost'
 import { Query } from 'react-apollo' 
-        
+
+const NEW_LINKS_SUBSCRIPTION = gql`
+    subscription {
+        newLink {
+            id
+            url
+            description
+            createdAt
+            postedBy {
+                id
+                name
+            }
+            votes {
+                id
+                user {
+                    id
+                }
+            }
+        }
+    }
+`
+
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`
+
 export const FEED_QUERY = gql`
     {
         feed {
@@ -36,12 +84,40 @@ class LinkList extends Component {
         store.writeQuery({query: FEED_QUERY, data})
     }
 
+    _subscribeToNewLinks = subscribeToMore => {
+        subscribeToMore({
+            document: NEW_LINKS_SUBSCRIPTION,
+            updateQuery: (prev, { subscriptionData }) => {
+                if (!subscriptionData.data) return prev
+                const newLink = subscriptionData.data.newLink
+                const exists = prev.feed.links.find(({id}) => id === newLink.id);
+                if (exists) return prev;
+
+                return Object.assign({}, prev, {
+                    feed: {
+                        links: [newLink, ...prev.feed.links],
+                        count: prev.feed.links.length + 1,
+                        __typename: prev.feed.__typename
+                    }
+                })
+            }
+        })
+    }
+
+    _subscribeToNewVotes = subscribeToMore => {
+        subscribeToMore({
+            document:NEW_VOTES_SUBSCRIPTION
+        })
+    }
     render() {
         return (
             <Query query={FEED_QUERY}>
-                {({ loading, error, data }) => {
+                {({ loading, error, data, subscribeToMore }) => {
                     if (loading) return <div> Fetching </div>
                     if (error) return <div> Error </div>
+
+                    this._subscribeToNewLinks(subscribeToMore)
+                    this._subscribeToNewVotes(subscribeToMore)
                     
                     const linksToRender = data.feed.links
 
